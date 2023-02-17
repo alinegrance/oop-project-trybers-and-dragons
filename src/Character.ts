d9dc048ad3aa00c161004899f3c4ad8ce4ada241
Aline Grance <grance.aline@gmail.com>
2023-02-17 16:59:03 -0300

    Add req 08

---
+++ src/Character.ts
@@ -0,0 +1,22 @@
+// import Archetype from './Archetypes';
+// import Energy from './Energy';
+// import Fighter from './Fighter';
+// import Race from './Races';
+
+// export default class Character implements Fighter {
+//   private _race: Race;
+//   private _archetype: Archetype;
+//   private maxLifePoints: number;
+//   private _lifePoints: number;
+//   private _strength: number;
+//   private _defense: number;
+//   private _dexterity: number;
+//   private _energy: Energy;
+//   private _name: string;
+
+//   constructor(name: string) {
+//     this._name = name;
+//     this._dexterity = Math.floor(Math.random() * 10) + 1;
+//     this._race = new Elf()
+//   }
+// }
